cc873c0c8606836bba6e9272c29e5f37c905582f
a19_deloitte <a19@deloitte.com>
2025-06-27 15:35:59 +0530

    Enhance User model by adding index to primary key

--- db.py
+++ db.py
@@ -25,7 +25,7 @@ class Document(Base):
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True,index=True)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
---
+++ local postgres.session.sql
@@ -0,0 +1,5 @@
+-- SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';
+-- SELECT * FROM pg_extension WHERE extname = 'vector';
+
+-- DROP TABLE IF EXISTS messages, chat_sessions, users, documents CASCADE;
+DROP TABLE IF EXISTS documents CASCADE;
